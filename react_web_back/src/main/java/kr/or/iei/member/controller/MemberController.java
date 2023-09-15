f6950ad99152aa6569957e4bb8a8723b68a85194
YeonJHw <142170249+YeonJHw@users.noreply.github.com>
2023-09-15 16:00:26 +0900

    Initial commit

---
+++ react_web_back/src/main/java/kr/or/iei/member/controller/MemberController.java
@@ -0,0 +1,10 @@
+package kr.or.iei.member.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController//비동기요청용 컨트롤러 @Responsebody 안붙여도 됨
+@RequestMapping(value = "/member")
+public class MemberController {
+	
+}
